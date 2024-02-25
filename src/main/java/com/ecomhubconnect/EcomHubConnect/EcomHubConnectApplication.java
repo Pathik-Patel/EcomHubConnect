c74c0fb6a8f39b3a7cf8c8be007c901529b19ac9
“Web <Web@digiwagon.com>
2024-02-24 20:21:44 -0500

    Initial Project Setup

---
+++ src/main/java/com/ecomhubconnect/EcomHubConnect/EcomHubConnectApplication.java
@@ -0,0 +1,13 @@
+package com.ecomhubconnect.EcomHubConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomHubConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomHubConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomhubconnect/EcomHubConnect/EcomHubConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomhubconnect.EcomHubConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomHubConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
