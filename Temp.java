3d366a025908bd54887212acd7e4610b897ec341
Pathik <pathikp901@gmail.com>
2025-05-15 14:51:08 -0400

    Integrated Redis as Well

---
+++ Temp.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.TimeUnit;
+
+public class Temp {
+    public static void main(String[] args) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("TimeUnit.SECONDS: " + TimeUnit.SECONDS);
+    }
+}
---
+++ src/main/java/com/ecomhubconnect/EcomHubConnect/Service/RedisService.java
@@ -0,0 +1,21 @@
+package com.ecomhubconnect.EcomHubConnect.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+	
+	@Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+}
